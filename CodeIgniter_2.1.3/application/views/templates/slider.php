d213c85f7fe710d451d3dd5b475c4fa64fe85603
juan carlos vargas godoy <juan.dvil@gmail.com>
2013-06-21 14:11:35 -0400

    slider

---
+++ CodeIgniter_2.1.3/application/views/templates/slider.php
@@ -0,0 +1,30 @@
+	<!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+	<div class="ws_images">
+		<ul>
+			<li><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/auditoria_condominios.jpg" alt="AUDITORIA CONDOMINIOS" title="AUDITORIA CONDOMINIOS" id="wows1_0"/></li>
+			<li><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/auditoria_financiera.jpg" alt="AUDITORIA FINANCIERA" title="AUDITORIA FINANCIERA" id="wows1_1"/></li>
+			<li><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/capacitacion.jpg" alt="capacitacion" title="capacitacion" id="wows1_2"/></li>
+			<li><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/contabilidad.jpg" alt="CONTABILIDAD" title="CONTABILIDAD" id="wows1_3"/></li>
+			<li><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/laboral.jpg" alt="LABORAL" title="LABORAL" id="wows1_4"/></li>
+			<li><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/legal.jpg" alt="LEGAL" title="LEGAL" id="wows1_5"/></li>
+			<li><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/tributaria.jpg" alt="tributaria" title="tributaria" id="wows1_6"/></li>
+		</ul>
+	</div>
+		<div class="ws_bullets">
+			<div>
+				<a href="#" title="AUDITORIA CONDOMINIOS"><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/auditoria_condominios.jpg" alt="AUDITORIA CONDOMINIOS"/>1</a>
+				<a href="#" title="AUDITORIA FINANCIERA"><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/auditoria_financiera.jpg" alt="AUDITORIA FINANCIERA"/>2</a>
+				<a href="#" title="capacitacion"><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/capacitacion.jpg" alt="capacitacion"/>3</a>
+				<a href="#" title="CONTABILIDAD"><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/contabilidad.jpg" alt="CONTABILIDAD"/>4</a>
+				<a href="#" title="LABORAL"><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/laboral.jpg" alt="LABORAL"/>5</a>
+				<a href="#" title="LEGAL"><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/legal.jpg" alt="LEGAL"/>6</a>
+				<a href="#" title="tributaria"><img src="/<?php echo config_item('dir_alias') ?>/img/img-slider/tributaria.jpg" alt="tributaria"/>7</a>
+			</div>
+		</div>
+		<span class="wsl"><a href="http://wowslider.com">Lightbox Slideshow</a> by WOWSlider.com v3.8</span>
+	<div class="ws_shadow"></div>
+</div>
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+	<!-- End WOWSlider.com BODY section -->
